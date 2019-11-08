c43e2b7ad497825b276a2a69054fac5bd62192a5
stronger_myself <misha.smerch.1997@gmail.com>
2019-11-08 17:04:18 +0800

    update sample: css modules

--- packages/sample/src/index.tsx
+++ packages/sample/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import * as ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import { Main } from './main'
 
 ReactDOM.render(<Main/>, document.getElementById('root'))
