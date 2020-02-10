34472797a1feb191e4c6dd3bd05db6eda7c86220
Михаил <msolovyev@itsumma.ru>
2020-02-10 21:08:23 +0800

    refact sample

--- packages/_sample/demo/main.tsx
+++ packages/_sample/demo/main.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react'
+import { Sample } from 'sample'
 
 interface Props {}
 
 export const Main: FunctionComponent<Props> = (props) => {
-    
     return (
-        <>Sample</>
+        <Sample/>
     )
 }
 
---
+++ packages/_sample/src/sample.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react'
+
+interface Props {}
+
+export const Sample: FunctionComponent<Props> = (props) => {
+    return (
+        <>Sample</>
+    )
+}
+
+Sample.displayName = 'Sample'
